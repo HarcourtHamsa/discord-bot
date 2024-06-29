ff98bd08ddc2be0154a868c6768ad2dc3f7d1fa7
Harcourt Hamsa <hamsaharcourt@gmail.com>
2024-06-29 23:40:00 +0100

    fix typo

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 const app = express()
 
 
-const PORT = process.env.PORT | 8000;
+const PORT = process.env.PORT || 8000;
 
 app.get('/', function (req, res) {
     res.send('Discord Bot Running')
